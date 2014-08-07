02f5ddd6d71eeb8651bfba6e9a92b3fc6d0fca70
Reza Rahman <reza_rahman@lycos.com>
2014-08-07 08:48:17 -0400

    Add more tests

--- src/test/java/org/glassfish/jms2lab/Jms2Test.java
+++ src/test/java/org/glassfish/jms2lab/Jms2Test.java
@@ -6,6 +6,8 @@ import javax.inject.Inject;
 import javax.jms.ConnectionFactory;
 import javax.jms.DeliveryMode;
 import javax.jms.JMSContext;
+import javax.jms.JMSException;
+import javax.jms.Message;
 import javax.jms.Queue;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
@@ -13,6 +15,8 @@ import org.jboss.arquillian.junit.InSequence;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -93,4 +97,29 @@ public class Jms2Test {
                     jmsContext.createConsumer(myQueue).receiveBody(byte[].class));
         }
     }
+
+    @Test
+    @InSequence(5)
+    public void testSendMessageWithProperties() throws JMSException {
+        // We are using a try with resources here, but in a Java EE
+        // environment we can directly inject a managed JMS context.
+        try (JMSContext jmsContext = myConnectionFactory.createContext()) {
+            jmsContext.createProducer()
+                    .setDisableMessageID(true)
+                    .setDisableMessageTimestamp(true)
+                    .setPriority(9)
+                    .setProperty("foo", "bar")
+                    .setProperty("foo2", "bar2")
+                    .send(myQueue, "message5");
+
+            Message message = jmsContext.createConsumer(myQueue).receive();
+
+            assertEquals(1, message.getIntProperty("JMSXDeliveryCount"));
+            assertEquals("bar", message.getStringProperty("foo"));
+            assertEquals("bar2", message.getStringProperty("foo2"));
+            assertEquals("message5", message.getBody(String.class));
+        }
+    }
+
+    // Delivery delay, request/response, MDB
 }
